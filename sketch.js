a7fff2b6726033cac2b6a74090587d3829ab6e4e
Dillon Brock <dillonbrock@dillons-mbp.lan>
2021-04-05 11:21:03 -0500

    creates pixelated version of image

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-let img;
-let imageSize;
-let d;
+// let img;
+// let imageSize;
+// let d;
 
 // function preload() {
 // 	img = loadImage('assets/bgBarstow.png');
@@ -27,65 +27,55 @@ let d;
 // function pixelPools() {
 // }
 
-
-let cols;
-let rows;
-let current; // = new float[cols][rows];
-let previous; // = new float[cols][rows];
-
-let dampening = 0.99;
+let img;
+let imageScale = 10;
+let colors = [];
 
 function preload() {
 	img = loadImage('assets/bgBarstow.png');
 }
 
 function setup() {
-  createCanvas(800, 800);
-  pixelDensity(1);
-  image(img, 0, 0, 800, 800);
-  //iterate through pixels of photo
-  //store a new array of pixel colors?
+	createCanvas(800, 800);
+	background(51);
+	pixelDensity(1);
+	img.resize(width/imageScale, height/imageScale);
+	img.loadPixels();
+	for (var y = 0; y < img.height; y++) {
+		for (var x = 0; x < img.width; x++) {
+			var index = (img.width - x - 1 + (y * img.width)) * 4;
+			var r = img.pixels[index+0];
+			var g = img.pixels[index+1];
+			var b = img.pixels[index+2];
+			var a = 255;
+			var col = color(r, g, b, a);
+			colors.push(col);		
+		}
+	}
+	console.log(colors.length);
 
-  cols = width;
-  rows = height;
-  // The following line initializes a 2D cols-by-rows array with zeroes
-  // in every array cell, and is equivalent to this Processing line:
-  // current = new float[cols][rows];
-  //arays of color values, all initialized to 0, which creates black background. 
-  current = new Array(cols).fill(0).map(n => new Array(rows).fill(0)); //
-  previous = new Array(cols).fill(0).map(n => new Array(rows).fill(0)); //
-}
+	for (var y = 0; y < height/imageScale; y++) {
+		for (var x = 0; x < width/imageScale; x++) {
+			var index = x + y * width/imageScale;
+			noStroke();
+			fill(colors[index]);
+			//circle(x*imageScale + imageScale/2, y*imageScale, imageScale);
+			rect(x*imageScale, y * imageScale, imageScale, imageScale);
+		}
+	}
 
-function mouseDragged() {
-  previous[mouseX][mouseY] = 500;
+
+
+
 }
 
 function draw() {
-  background(img);
 
-  loadPixels();
-  for (let i = 1; i < cols - 1; i++) {
-    for (let j = 1; j < rows - 1; j++) {
-      current[i][j] =
-        (previous[i - 1][j] +
-          previous[i + 1][j] +
-          previous[i][j - 1] +
-          previous[i][j + 1]) /
-          2 -
-        current[i][j];
-      current[i][j] = current[i][j] * dampening;
-      // Unlike in Processing, the pixels array in p5.js has 4 entries
-      // for each pixel, so we have to multiply the index by 4 and then
-      // set the entries for each color component separately.
-      let index = (i + j * cols) * 4;
-      pixels[index + 0] = current[i][j];
-      pixels[index + 1] = current[i][j];
-      pixels[index + 2] = current[i][j];
-    }
-  }
-  updatePixels();
-
-  let temp = previous;
-  previous = current;
-  current = temp;
+	loadPixels();
+	for (let i = 0; i < (4*width*height); i++) {
+		pixels[i] = pixels[i+4];
+	}
+	updatePixels();
+	
 }
+
